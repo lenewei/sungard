c3ef65e286c41167fb6df8c64ced03acf9257006
Steven Xu <nonumber1989@gmail.com>
2015-06-06 13:36:37 +0800

    add some code for scanner

--- hawk/www/js/app.js
+++ hawk/www/js/app.js
@@ -4,68 +4,69 @@
 // 'hawk' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'hawk.controllers' is found in controllers.js
-angular.module('hawk', ['ionic', 'ngCordova', 'ngResource', 'ngStorage', 'pascalprecht.translate', 'angular-jwt'])
+angular.module('hawk', ['ionic', 'ngCordova', 'ngResource', 'ngStorage', 'pascalprecht.translate', 'angular-jwt', 'monospaced.qrcode'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'HawkController'
-  })
-
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
-
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlayListController'
-        }
-      }
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlayListController'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/menu.html",
+                controller: 'HawkController'
+            })
+
+            .state('app.search', {
+                url: "/search",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/search.html",
+                        controller: 'SearchController'
+                    }
+                }
+            })
+
+            .state('app.browse', {
+                url: "/browse",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/browse.html"
+                    }
+                }
+            })
+            .state('app.playlists', {
+                url: "/playlists",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/playlists.html",
+                        controller: 'PlayListController'
+                    }
+                }
+            })
+
+            .state('app.single', {
+                url: "/playlists/:playlistId",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/playlist.html",
+                        controller: 'PlayListController'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/playlists');
+    });
---
+++ hawk/www/js/controllers/searchController.js
@@ -0,0 +1,70 @@
+/**
+ * Created by seven on 6/6/2015.
+ */
+var hawk = angular.module('hawk');
+
+hawk.controller('SearchController', function ($scope, $ionicModal,$cordovaBarcodeScanner) {
+    $scope.user = {};
+
+    $scope.generateQRCode = function () {
+        //var theQRString ="";
+        //angular.forEach($scope.user, function(value, key) {
+        //    theQRString =  theQRString.concat(key+":"+value+",");
+        //});
+        //$scope.QR = theQRString.slice(0,theQRString.lastIndexOf(","));
+        $scope.QR = "yeyeyey:yeyyey,"
+    }
+
+    $ionicModal.fromTemplateUrl('templates/QRCodeDialog.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+    $scope.openModal = function () {
+        $scope.generateQRCode();
+        $scope.modal.show();
+    };
+    $scope.closeModal = function () {
+        $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function () {
+        $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function () {
+        // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function () {
+        // Execute action
+    });
+
+    $ionicPlatform.ready(function () {
+        $scope.scanBarcode = function () {
+            $cordovaBarcodeScanner
+                .scan()
+                .then(function (barcodeData) {
+                    // Success! Barcode data is here
+                    alert(barcodeData.text);
+                    console.log("Barcode Format -> " + barcodeData.format);
+                    console.log("Cancelled -> " + barcodeData.cancelled);
+                }, function (error) {
+                    // An error occurred
+                    console.log("An error happened -> " + error);
+                });
+        }
+
+        $scope.enCodeBarcode = function () {
+            // NOTE: encoding not functioning yet
+            $cordovaBarcodeScanner
+                .encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nonumber1989.com")
+                .then(function (success) {
+                    // Success!
+                }, function (error) {
+                    // An error occurred
+                });
+        }
+    });
+});
